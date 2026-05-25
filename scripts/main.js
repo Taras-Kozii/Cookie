a685bb052edbe430f177e54cbc847fd53061fa98
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2026-05-25 21:27:09 +0000

    header done

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-// import { initNavigation } from './menu.js';
+import { initNavigation } from './menu.js';
 import { initHeader } from './header.js';
 // import { VanillaTilt } from './effects/tilt/vanilla-tilt.js';
 // import SimpleParallax from './effects/simpleParallax/index.js';
 // import { watcherAnim, watcherToggle, countAnimate } from './animation.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // initNavigation();
+  initNavigation();
   initHeader();
   // new SimpleParallax(document.querySelectorAll('.parallax-img'), {
   //   delay: 0.6,
