2b3e1f14b5c696b2433d08c8b0fb75589f0d5d75
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2026-08-11 08:51:09 +0000

    add anim about

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,6 @@ import { initForms } from './forms.js';
 import { initSpollers } from './spollers.js';
 import { watcherToggle } from './effects/animation.js';
 
-// import { VanillaTilt } from './effects/tilt/vanilla-tilt.js';
-// import SimpleParallax from './effects/simpleParallax/index.js';
-// import { watcherAnim, watcherToggle, countAnimate } from './animation.js';
-
 document.addEventListener('DOMContentLoaded', () => {
   initNavigation();
   initHeader();
@@ -21,4 +17,5 @@ document.addEventListener('DOMContentLoaded', () => {
   watcherToggle('.socials__list.scroll-anim a', 0.2);
   watcherToggle('.button', 0.2);
   watcherToggle('.how__item.scroll-anim', 0.5);
+  watcherToggle('.card-values.scroll-anim', 0.35);
 });
